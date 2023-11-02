3ab7fdb4e0d0ee6a10a6fd1c731995e217d9e42e
krishna <krishnasaiprakhya@gmail.com>
2023-11-02 15:22:55 +0530

    responsive button

--- basic.js
+++ basic.js
@@ -6,3 +6,24 @@ Email --> Can be duplicate
 All Fields are required
 None must be empty
 */
+let arr=100
+let info=document.getElementById("main-info");
+let mainContainer=document.getElementById("MainContainer");
+let submitBtn=document.getElementById("submit");
+let resetBtn=document.getElementById("reset");
+function addInfo(){
+    arr+=1;
+    let formNo=document.createElement("p");
+    let paraInfo=document.createElement("p");
+    let personInfo=document.createElement("p");
+    formNo.textContent="form No.:"+arr;
+    paraInfo.textContent="Your Form Has Been Submitted";
+    personInfo.textContent=
+    info.appendChild(paraInfo);
+    info.appendChild(formNo);
+
+}
+submitBtn.onclick=function(){
+    addInfo();
+    
+}
